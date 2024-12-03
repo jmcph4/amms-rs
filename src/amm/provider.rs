ccc21e20caad19c4178ab033a2b643488c066973
jmcph4 <jmcph4.github@gmail.com>
2024-12-03 16:34:40 +1000

    Add `AlloyProvider` and implement traits on it

--- src/amm/provider.rs
+++ src/amm/provider.rs
@@ -1,6 +1,11 @@
-use std::future::Future;
+use std::future::{Future, IntoFuture};
 
-use alloy::primitives::{Address, StorageKey, StorageValue};
+use alloy::{
+    network::Network,
+    primitives::{Address, StorageKey, StorageValue},
+    providers::Provider,
+    transports::{RpcError, Transport, TransportErrorKind},
+};
 
 /// Provides arbitrary state from an EVM chain
 pub trait BlockchainDataProvider {
@@ -8,7 +13,29 @@ pub trait BlockchainDataProvider {
 
     /// Returns the storage value for the given address at the given storage slot
     fn storage_slot_at(
+        &self,
         address: Address,
         slot: StorageKey,
-    ) -> impl Future<Output = Result<StorageValue, Self::Error>> + Send;
+    ) -> impl Future<Output = Result<StorageValue, Self::Error>>;
+}
+
+/// Newtype wrapper around Alloy's [`Provider`] trait
+///
+/// Sadly necessary due to the requirement to implement [`BlockchainDataProvider`]
+pub struct AlloyProvider<T: Transport + Clone, N: Network>(pub Box<dyn Provider<T, N>>);
+
+impl<T, N> BlockchainDataProvider for AlloyProvider<T, N>
+where
+    T: Transport + Clone,
+    N: Network,
+{
+    type Error = RpcError<TransportErrorKind>;
+
+    fn storage_slot_at(
+        &self,
+        address: Address,
+        slot: StorageKey,
+    ) -> impl Future<Output = Result<StorageValue, Self::Error>> {
+        self.0.get_storage_at(address, slot.into()).into_future()
+    }
 }
