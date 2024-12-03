13bd96f3952ffef170acb38b0d783a64a9701dcc
jmcph4 <jmcph4.github@gmail.com>
2024-12-03 16:02:12 +1000

    Use Alloy types

--- src/amm/provider.rs
+++ src/amm/provider.rs
@@ -1,20 +1,14 @@
 use std::future::Future;
 
-use alloy::primitives::{Address, U256};
-
-/// EVM word
-pub type Word = U256;
-
-/// A **storage slot** in the EVM storage tree (not a consensus slot!)
-pub type Slot = u64;
+use alloy::primitives::{Address, StorageKey, StorageValue};
 
 /// Provides arbitrary state from an EVM chain
 pub trait BlockchainDataProvider {
     type Error;
 
-    /// Returns the storage value for the given address at the given **storage slot**
+    /// Returns the storage value for the given address at the given storage slot
     fn storage_slot_at(
         address: Address,
-        slot: Slot,
-    ) -> impl Future<Output = Result<Word, Self::Error>> + Send;
+        slot: StorageKey,
+    ) -> impl Future<Output = Result<StorageValue, Self::Error>> + Send;
 }
